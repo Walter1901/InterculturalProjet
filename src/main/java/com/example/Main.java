0656d0146f6b1a3144b9157ecbbddb31475ba6cc
Rojin Dalir <rojin.dalirkanesbi@students.hevs.ch>
2025-04-14 15:10:06 +0200

    refactor: ignore .vscode/

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import com.example.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello User\nFinance Tracker");
+        Menu.showMainMenu(); // Startet das Hauptmenü
+    }
+}
