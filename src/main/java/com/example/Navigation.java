c1b37ea8c4fe85453a4f9a482125982dbb887a0d
Rojin Dalir <rojin.dalirkanesbi@students.hevs.ch>
2025-04-14 15:11:36 +0200

    refactor: ignore .vscode/

--- src/main/java/com/example/Navigation.java
+++ src/main/java/com/example/Navigation.java
@@ -1,4 +1,9 @@
 package com.example;
 
 public class Navigation {
+    public static void showNavigation() {
+        System.out.println("\nNavigation:");
+        System.out.println("Home    Stock    Account");
+        // Logik für Navigation zwischen den Bereichen
+    }
 }
