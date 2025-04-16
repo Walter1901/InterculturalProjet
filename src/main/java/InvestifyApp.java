37d90b06dccff4d74c3719df6c41978bfa89fa51
Andre Santos Leal <andre.santosle@hes-so.ch>
2025-04-16 10:44:24 +0200

    added methods for my app

--- src/main/java/InvestifyApp.java
+++ src/main/java/InvestifyApp.java
@@ -9,8 +9,21 @@ public class InvestifyApp {
         investifyApp.setBackground(phoneUtils.backgroundColor);
         investifyApp.setLayout(new GridLayout(6, 1));
 
-
         // Nav bar
+        JPanel investifyNavBar = createNavBarInvestify();
+
+        investifyApp.add(new JLabel());
+        investifyApp.add(new JLabel());
+        investifyApp.add(new JLabel());
+        investifyApp.add(new JLabel());
+        investifyApp.add(new JLabel());
+        investifyApp.add(investifyNavBar);
+
+        return investifyApp;
+    }
+
+    public static JPanel createNavBarInvestify(){
+
         JPanel investifyNavBar = new JPanel();
         investifyNavBar.setBackground(phoneUtils.backgroundColor);
         investifyNavBar.setLayout(new GridLayout(1, 5));
@@ -21,34 +34,27 @@ public class InvestifyApp {
         homeLabel.setIcon(new ImageIcon("src/main/resources/investifyIcons/homeIcon.png"));
         investifyNavBar.add(homeLabel, BorderLayout.SOUTH);
 
-        JLabel searchLabel = new JLabel("Search");
+        JLabel searchLabel = new JLabel();
         searchLabel.setHorizontalAlignment(SwingConstants.CENTER);
         searchLabel.setIcon(new ImageIcon("src/main/resources/investifyIcons/searchIcon.png"));
         investifyNavBar.add(searchLabel, BorderLayout.SOUTH);
 
-        JLabel portfolioLabel = new JLabel("Portfolio");
+        JLabel portfolioLabel = new JLabel();
         portfolioLabel.setHorizontalAlignment(SwingConstants.CENTER);
         portfolioLabel.setIcon(new ImageIcon("src/main/resources/investifyIcons/portfolioIcon.png"));
         investifyNavBar.add(portfolioLabel, BorderLayout.SOUTH);
 
-        JLabel recurrentLabel = new JLabel("Recurrent");
+        JLabel recurrentLabel = new JLabel();
         recurrentLabel.setHorizontalAlignment(SwingConstants.CENTER);
         recurrentLabel.setIcon(new ImageIcon("src/main/resources/investifyIcons/recurrentIcon.png"));
         investifyNavBar.add(recurrentLabel, BorderLayout.SOUTH);
 
-        JLabel accountLabel = new JLabel("Account");
+        JLabel accountLabel = new JLabel();
         accountLabel.setHorizontalAlignment(SwingConstants.CENTER);
         accountLabel.setIcon(new ImageIcon("src/main/resources/investifyIcons/accountIcon.png"));
         investifyNavBar.add(accountLabel, BorderLayout.SOUTH);
 
-        investifyApp.add(new JLabel());
-        investifyApp.add(new JLabel());
-        investifyApp.add(new JLabel());
-        investifyApp.add(new JLabel());
-        investifyApp.add(new JLabel());
-        investifyApp.add(investifyNavBar);
-
-        return investifyApp;
+        return investifyNavBar;
     }
 
 }
