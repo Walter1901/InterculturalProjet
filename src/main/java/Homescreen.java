6162ed57581edc2ea4dea218a48227c9b12af552
Andre Santos Leal <andre.santosle@hes-so.ch>
2025-05-18 12:26:41 +0200

    added comments to the homescreen class as well as my investify app + small bug fixes

--- src/main/java/Homescreen.java
+++ src/main/java/Homescreen.java
@@ -1,8 +1,15 @@
-import javax.swing.*;
-
-public class Homescreen extends phoneUtils{
+/**
+ * Main class for the phone application.
+ * This class extends phoneUtils and serves as the entry point for the application.
+ * It creates and displays the home screen of the phone interface.
+ */
+public class Homescreen extends phoneUtils {
+    /**
+     * Main method that starts the application.
+     * When executed, this method calls createHomescreen() to initialize
+     * and display the phone's user interface.
+     */
     public static void main(String[] args) {
-        createHomescreen(); // Creates the phone
+        createHomescreen(); // Creates and displays the phone interface
     }
-
 }
